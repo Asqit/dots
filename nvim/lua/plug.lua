145064fae01c5d78e4b02882cc41eae06007a97c
Ondřej Tuček <ondrejtucek9@gmail.com>
2023-03-03 22:40:07 +0100

    Neovim tunning
    Started integration of Linters, Formatters, LSPs...for neovim
    
    - Added Mason plugin
    - Added rust-tools
    - Added TypeScript / JavaScript LSP

---
+++ nvim/lua/plug.lua
@@ -0,0 +1,38 @@
+--> plug.lua
+
+
+return require("packer").startup(function(use)
+
+	-- File tree
+	use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			'nvim-tree/nvim-web-devicons', -- optional, for file icons
+		},
+	}
+
+
+	-- Nice looking status bar
+	use {
+		'nvim-lualine/lualine.nvim',
+		 requires = { 
+			 'kyazdani42/nvim-web-devicons', 
+			 opt = true 
+		 }
+	 }
+
+
+	-- Gruvbox is a retro lookin' theme
+	use { "ellisonleao/gruvbox.nvim" }
+
+	-- Package manager (LSP, Linters...)
+	use {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	}
+
+	-- Rust tools
+	use 'simrat39/rust-tools.nvim'
+
+end)
---
+++ nvim/lua/vars.lua
@@ -0,0 +1,8 @@
+--> vars.lua
+
+-- Creating a shortcut (instead of typing the whole table name, we simply type g)
+local g = vim.g
+
+g.t_co = 256				-- Enables support for 256 colors (for those sweeet sweet themes)
+g.background = "dark"		-- Defaulty stick with dark mode pweeease
+
